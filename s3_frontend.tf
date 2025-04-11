039233535ddfc819a1413e77c94b103b4473833d
sunillangan <58847457+sunillangan@users.noreply.github.com>
2025-04-11 09:59:37 +0800

    Update s3_frontend.tf

--- s3_frontend.tf
+++ s3_frontend.tf
@@ -5,3 +5,30 @@ resource "aws_s3_bucket" "frontend" {
     error_document = "error.html"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "frontend_block" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.frontend.arn}/*"
+    }]
+  })
+}
+
+output "website_url" {
+  value = aws_s3_bucket.frontend.website_endpoint
+}
